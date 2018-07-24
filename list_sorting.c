2c851a43efdb4aa95b0264901005ef7185bc3321
Dylan Slogrove <dslogrove@gmail.com>
2018-07-24 14:48:56 +0200

    Reimplimented list reversal

--- list_sorting.c
+++ list_sorting.c
@@ -6,7 +6,7 @@
 /*   By: dslogrov <dslogrove@gmail.com>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/06/24 14:55:11 by dslogrov          #+#    #+#             */
-/*   Updated: 2018/07/24 11:28:30 by dslogrov         ###   ########.fr       */
+/*   Updated: 2018/07/24 14:43:26 by dslogrov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,22 +26,20 @@ t_list	*sort_file_list(t_list *lst, t_flag flags)
 	if (flags & FLAG_LT)
 	{
 		if (flags & FLAG_LC)
-			ft_lstsort(&lst, (long (*)(const void *, const void *))&compare_lc,
-				rev);
+			ft_lstsort(&lst, (long (*)(const void *, const void *))&compare_lc);
 		else if (flags & FLAG_LU)
-			ft_lstsort(&lst, (long (*)(const void *, const void *))&compare_lu,
-				rev);
+			ft_lstsort(&lst, (long (*)(const void *, const void *))&compare_lu);
 		else if (flags & FLAG_UU)
-			ft_lstsort(&lst, (long (*)(const void *, const void *))&compare_uu,
-				rev);
+			ft_lstsort(&lst, (long (*)(const void *, const void *))&compare_uu);
 		else
-			ft_lstsort(&lst, (long (*)(const void *, const void *))&compare_lt,
-				rev);
+			ft_lstsort(&lst, (long (*)(const void *, const void *))&compare_lt);
 	}
 	else if (flags & FLAG_US)
-		ft_lstsort(&lst, (long (*)(const void *, const void *))&compare_us, rev);
+		ft_lstsort(&lst, (long (*)(const void *, const void *))&compare_us);
 	else
-		ft_lstsort(&lst, (long (*)(const void *, const void *))&compare_default,
-			rev);
+		ft_lstsort(&lst,
+			(long (*)(const void *, const void *))&compare_default);
+	if (rev)
+		ft_lstrev(&lst);
 	return (lst);
 }
